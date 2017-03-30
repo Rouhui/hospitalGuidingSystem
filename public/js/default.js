26c2bb9baee395df389769b96dea1f6a4c222184
Ruohui <Ruohui@gmail.com>
2017-03-30 20:23:41 +0800

    common part is partially completed

---
+++ public/js/default.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+});
+
+function login() {
+    $('#mask').css('display', 'block');
+    $('#login-dialog').css('display', 'block');
+}
+
+function cancelLogin(){
+    $('#mask').css('display', 'none');
+    $('#login-dialog').css('display', 'none');
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('common.layouts')
+
+@section('content')
+    <div class="col-md-3 menu-list">
+        <div class="list-group">
+            <li><a href="#"><i class="icon-stethoscope"></i>智能导诊</a></li>
+            <li><a href="#"><i class="icon-user-md"></i>门诊预约</a></li>
+            <li><a href="#"><i class="icon-stethoscope"></i>价格查询</a></li>
+            <li><a href="#"><i class="icon-stethoscope"></i>费用清单查询</a></li>
+            <li><a href="#"><i class="icon-list-alt"></i>检查单/检验单查询</a><
+            <li><a href="#"><i class="icon-print"></i>打印</a>
+        </div>
+    </div>
+    <div class="col-md-9"></div>
+@stop
