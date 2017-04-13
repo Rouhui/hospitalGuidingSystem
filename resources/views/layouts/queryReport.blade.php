dbbaa23b51611e34766665cd2d66a7fecca517f7
Ruohui <Ruohui@gmail.com>
2017-04-13 14:16:48 +0800

    add report page

---
+++ resources/views/layouts/queryReport.blade.php
@@ -0,0 +1,57 @@
+<div class="report-content">
+    <div class="report-nav">
+        <div class="title">报告查询</div>
+        <div class="tip"><a href="#">如何获取医嘱号</a></div>
+    </div>
+
+    <div>
+        <form class="form-inline" role="form">
+            <div class="left-part">
+            <div class="radio">
+                <label>
+                    <input type="radio" name="optionsRadios" id="jiancha" value="jiancha" checked>检查单
+                </label>
+            </div>
+            <div class="radio">
+                <label>
+                    <input type="radio" name="optionsRadios" id="jianyan" value="jianyan">检验单
+                </label>
+            </div>
+            </div>
+            <div class="right-part">
+                <div class="form-group">
+                    <label for="name">姓名：</label>
+                    <input type="text" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="no">医嘱号：</label>
+                    <input type="text" id="no">
+                </div>
+                <button type="submit">查询</button>
+            </div>
+        </form>
+    </div>
+
+    <div style="padding-bottom: 20px;">
+        <div class="info-item">
+            <div class="basic-info">姓名：<span></span></div>
+            <div class="basic-info">性别：</div>
+            <div class="basic-info">年龄：</div>
+            <div class="basic-info">医嘱号码：<span></span></div>
+            <div class="basic-info" style="width: auto">检查项目：<span></span></div>
+        </div>
+        <div class="divider"></div>
+        <div class="info-item">
+            <div class="xmjg">项目结果：</div><div class="xmjg2"><span>暂无此报告单信息，请核对信息后再重试！</span></div>
+            <div class="clear"></div>
+            <div class="xmjg" style="margin-top: 20px;">诊断结论：</div><div class="xmjg2"><span></span></div>
+        </div>
+        <div class="divider"></div>
+        <div class="info-item">
+            <div class="basic-info1">报告书写：</div>
+            <div class="basic-info1">审核医生：</div>
+            <div class="basic-info1">报告时间：</div>
+            <div class="basic-info1">审核时间：</div>
+        </div>
+    </div>
+</div>
